6a293e63ee0f996e00490ebfdf777903030acbda
ashen-vaults <alexhollander522@gmai.com>
2018-01-13 18:08:17 -0500

    added comments and autopep8

--- replication/genome_lib.py
+++ replication/genome_lib.py
@@ -1,5 +1,7 @@
 def pattern_matching(text, pattern):
-    return [[x, str(len(pattern) + x)] for x in range(len(text) - len(pattern) + 1) if pattern in text[x:len(pattern) + x]]
+    """ Returns the start and end positions of each instance of the pattern  """
+    return [[x, str(len(pattern) + x)] for x in range(len(text) - len(pattern) + 1) if
+            pattern in text[x:len(pattern) + x]]
 
 
 def frequent_words(text, k):
@@ -15,13 +17,17 @@ def reverse_complement(pattern):
 
 
 def find_invert_repeats(text, pattern_size):
+    """ Returns the overlap for frequent words and its reverse """
     freqs = frequent_words(text, pattern_size)
     rev_freqs = frequent_words(reverse_complement(text), pattern_size)
-    data = [[x, str(pattern_matching(text,x))] for x in set(freqs).intersection(rev_freqs)]
-    return data
+    return [[x, pattern_matching(text, x)] for x in set(freqs).intersection(rev_freqs)]
+
+
+def beautify_invert_repeats(repeats):
+    return "Match: {0} at [{1}:{2}]".format(repeats[0][0], repeats[0][1][0][0], repeats[0][1][0][1])
 
 
 #print([x[0] for x in pattern_matching("GATATATGCATATACTT", "ATAT")])
-#print(frequent_words("BABBASDCABCBABDDASDBBCASDBAB", 3))
-#print(reverse_complement("AGGGTTTCCCTGACCTTCACTGCAGGTCATGCA"))  # -> ACCGGGTTTT
+# print(frequent_words("BABBASDCABCBABDDASDBBCASDBAB", 3))
+# print(reverse_complement("AGGGTTTCCCTGACCTTCACTGCAGGTCATGCA"))  # -> ACCGGGTTTT
 print(find_invert_repeats("AGGGTTTCCCTGACCTTCACTGCAGGTCATGCA", 6))
